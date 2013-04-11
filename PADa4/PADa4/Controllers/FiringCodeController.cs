5cafd0eac49ba0cc3774868ab5cc4f3261493973
Nick Whitney <woofer_4444@hotmail.com>
2013-04-10 21:45:44 -0400

    Initial commit

---
+++ PADa4/PADa4/Controllers/FiringCodeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PADa4.Controllers
+{
+    public class FiringCodeController : ApiController
+    {
+        public string CheckCode(string code)
+        {
+            string ret = "DRILL";
+            if (code == "AZ035T29Z1984")
+            {
+                ret = "AUTHENTICATED";
+            }
+            return ret;
+        }
+    }
+}
---
+++ PADa4/PADa4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PADa4.WebApiApplication" Language="C#" %>
